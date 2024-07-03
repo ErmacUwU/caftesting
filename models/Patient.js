c542c78f30fecaa1f2366861aaf0f281335c4b0e
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2024-07-02 21:00:21 -0700

    Añadiendo los IDs en los esquemas

--- models/Patient.js
+++ models/Patient.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const PatientSchema = new mongoose.Schema(
   {
+    idPatient:{
+      type: String,
+      required: true,
+    },
     firstName: {
       type: String,
       required: true,
