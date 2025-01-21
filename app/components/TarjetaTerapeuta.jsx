4bda466c1eec959673f1ee691bf52f2f0068b8aa
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2025-01-20 16:55:29 -0800

    Cambiando la ruta (CORS)

--- app/components/TarjetaTerapeuta.jsx
+++ app/components/TarjetaTerapeuta.jsx
@@ -11,9 +11,7 @@ const TarjetaTerapeuta = () => {
   useEffect(() => {
     const getTherapists = async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/therapist", {
-          cache: "no-store",
-        });
+        const res = await fetch("/api/therapist", { cache: "no-store" });
 
         if (!res.ok) {
           throw new Error("Failed to fetch therapists");
