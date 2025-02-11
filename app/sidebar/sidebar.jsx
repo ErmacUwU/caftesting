44453d756b2e564551f68c71b803c9e9406b920d
Gloria Berenice Hernandez <>
2025-02-10 19:23:52 -0600

    Porfin voy a subir cambios

--- app/sidebar/sidebar.jsx
+++ app/sidebar/sidebar.jsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 import {
   HomeIcon,
   UserPlusIcon,
@@ -15,10 +15,10 @@ import {
   DocumentTextIcon,
   UserGroupIcon,
   Cog6ToothIcon,
-  DocumentIcon 
-} from '@heroicons/react/24/outline';
-import { User } from 'lucide-react';
-import { useAuth } from '../context/AuthContext.js'; // Importa el contexto de autenticación.
+  DocumentIcon,
+} from "@heroicons/react/24/outline";
+import { User } from "lucide-react";
+import { useAuth } from "../context/AuthContext.js"; // Importa el contexto de autenticación.
 
 const Sidebar = () => {
   // Extrae el estado de autenticación y la función logout desde el contexto de autenticación.
@@ -29,7 +29,7 @@ const Sidebar = () => {
       <div className="p-4 text-2xl font-semibold">CAF TESTS</div>
       <nav className="mt-8 flex-1">
         <ul>
-        <li className="mb-4">
+          <li className="mb-4">
             <Link
               href="/"
               className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
@@ -51,146 +51,147 @@ const Sidebar = () => {
             </li>
           )}
 
-        {/* Opciones visibles solo si el usuario está autenticado */}
-        {isAuthenticated && (
-          <>
-          {/* Opción de Cerrar Sesión */}
-          <li className="mb-4">
-          <button
-            onClick={logout}
-            className="w-full px-4 py-2 hover:bg-gray-700 rounded flex items-center text-left"
-          >
-            <User className="h-5 w-5 mr-3" />
-            Cerrar Sesión
-          </button>
-        </li>
+          {/* Opciones visibles solo si el usuario está autenticado */}
+          {isAuthenticated && (
+            <>
+              {/* Opción de Cerrar Sesión */}
+              <li className="mb-4">
+                <button
+                  onClick={logout}
+                  className="w-full px-4 py-2 hover:bg-gray-700 rounded flex items-center text-left"
+                >
+                  <User className="h-5 w-5 mr-3" />
+                  Cerrar Sesión
+                </button>
+              </li>
 
-          <li className="mb-4">
-            <Link
-              href="/registropacientes"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <UserPlusIcon className="h-5 w-5 mr-3" />
-              Registro Clientes
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/registroterapeuta"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <UsersIcon className="h-5 w-5 mr-3" />
-              Registro Terapeutas
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/citas"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <CalendarDaysIcon className="h-5 w-5 mr-3" />
-              Citas
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/agendas"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <ClipboardDocumentListIcon className="h-5 w-5 mr-3" />
-              Agendas
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/terapeuta"
-              className="px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <UsersIcon className="h-5 w-5 mr-3" />
-              Terapeuta
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/pacientes"
-              className="px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <UsersIcon className="h-5 w-5 mr-3" />
-              Pacientes
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/mensajes"
-              className="px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <ChatBubbleLeftRightIcon className="h-5 w-5 mr-3" />
-              Mensajes
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/reporte"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <DocumentChartBarIcon className="h-5 w-5 mr-3" />
-              Reportes
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/pagos"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <CreditCardIcon className="h-5 w-5 mr-3" />
-              Pagos
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/almacen"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <ArchiveBoxIcon className="h-5 w-5 mr-3" />
-              Almacen
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/documentador"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <DocumentTextIcon className="h-5 w-5 mr-3" />
-              Documentador
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/usuarios"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <UserGroupIcon className="h-5 w-5 mr-3" />
-              Usuarios
-            </Link>
-          </li>
-          <li className="mb-4">
-            <Link
-              href="/ajustes"
-              className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-              <Cog6ToothIcon className="h-5 w-5 mr-3" />
-              Ajustes
-            </Link>
-          </li>
-          <li>
-            <Link href="/docs"
-            className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
-            >
-               <DocumentIcon className="h-5 w-5 mr-3" />
-               Documentos
-            </Link>
-          </li>
-        </>
+              <li className="mb-4">
+                <Link
+                  href="/registropacientes"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <UserPlusIcon className="h-5 w-5 mr-3" />
+                  Registro Clientes
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/registroterapeuta"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <UsersIcon className="h-5 w-5 mr-3" />
+                  Registro Terapeutas
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/citas"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <CalendarDaysIcon className="h-5 w-5 mr-3" />
+                  Citas
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/agendas"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <ClipboardDocumentListIcon className="h-5 w-5 mr-3" />
+                  Agendas
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/terapeuta"
+                  className="px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <UsersIcon className="h-5 w-5 mr-3" />
+                  Terapeuta
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/pacientes"
+                  className="px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <UsersIcon className="h-5 w-5 mr-3" />
+                  Pacientes
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/mensajes"
+                  className="px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <ChatBubbleLeftRightIcon className="h-5 w-5 mr-3" />
+                  Mensajes
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/reporte"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <DocumentChartBarIcon className="h-5 w-5 mr-3" />
+                  Reportes
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/pagos"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <CreditCardIcon className="h-5 w-5 mr-3" />
+                  Pagos
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/almacen"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <ArchiveBoxIcon className="h-5 w-5 mr-3" />
+                  Almacen
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/documentador"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <DocumentTextIcon className="h-5 w-5 mr-3" />
+                  Documentador
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/usuarios"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <UserGroupIcon className="h-5 w-5 mr-3" />
+                  Usuarios
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  href="/ajustes"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <Cog6ToothIcon className="h-5 w-5 mr-3" />
+                  Ajustes
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/docs"
+                  className=" px-4 py-2 hover:bg-gray-700 rounded flex items-center"
+                >
+                  <DocumentIcon className="h-5 w-5 mr-3" />
+                  Documentos
+                </Link>
+              </li>
+            </>
           )}
         </ul>
       </nav>
