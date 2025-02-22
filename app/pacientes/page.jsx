b617256c57f9a7a89d5f942a7c5f4b7343226146
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2025-02-22 15:31:22 -0800

    Implementando rutas protegidas (Pacientes-Terapeutas)

--- app/pacientes/page.jsx
+++ app/pacientes/page.jsx
@@ -2,9 +2,25 @@
 
 import React, { useState, useEffect } from "react";
 import TarjetaPaciente from '../components/TarjetaPaciente'
-
+import { useAuth } from "../context/AuthContext.js"; 
+import { useRouter } from "next/navigation";
 
 const Pacientes = () => {
+
+  const { isAuthenticated } = useAuth(); // Obtiene el estado de autenticación
+  const router = useRouter(); // Hook para manejar redirecciones.
+  
+  // Redirige al login si no está autenticado
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login"); // Redirige a la página de inicio de sesión.
+    }
+  }, [isAuthenticated, router]);
+  
+  // Evita que se muestre contenido mientras redirige
+  if (!isAuthenticated) {
+    return null;
+  }
 
   return (
    <div>
--- app/terapeuta/page.jsx
+++ app/terapeuta/page.jsx
@@ -1,9 +1,25 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import TarjetaTerapeuta from '../components/TarjetaTerapeuta'
+import { useAuth } from "../context/AuthContext.js"; 
+import { useRouter } from "next/navigation";
 
 const Terapeutas = () => {
 
+  const { isAuthenticated } = useAuth(); // Obtiene el estado de autenticación
+  const router = useRouter(); // Hook para manejar redirecciones.
+  
+  // Redirige al login si no está autenticado
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login"); // Redirige a la página de inicio de sesión.
+    }
+  }, [isAuthenticated, router]);
+  
+  // Evita que se muestre contenido mientras redirige
+  if (!isAuthenticated) {
+    return null;
+  }
 
   return (
     <div>
