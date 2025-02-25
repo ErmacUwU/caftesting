f1e3f381c6fed3f98487ace396b8c81834fba300
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2025-02-24 18:46:46 -0800

    Implementando rutas protegidas (UseEffect)

--- app/ajustes/page.jsx
+++ app/ajustes/page.jsx
@@ -1,8 +1,24 @@
 "use client"
 import { useEffect, useState } from 'react';
+import { useAuth } from "../context/AuthContext.js"; 
+import { useRouter } from "next/navigation";
 
 const Ajustes = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
+    const { isAuthenticated } = useAuth(); // Obtiene el estado de autenticación
+      const router = useRouter(); // Hook para manejar redirecciones.
+    
+      // Redirige al login si no está autenticado
+      useEffect(() => {
+        if (!isAuthenticated) {
+          router.push("/login"); // Redirige a la página de inicio de sesión.
+        }
+      }, [isAuthenticated, router]);
+    
+      // Evita que se muestre contenido mientras redirige
+      if (!isAuthenticated) {
+        return null;
+      }
 
     useEffect(() => {
         // Verifica y aplica el modo guardado en localStorage
