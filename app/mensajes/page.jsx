8c06bc9ebe879169b63f84718a3e4014899bdf29
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2025-02-24 18:42:20 -0800

    Implementando rutas protegidas (Mensajes,Almacen,Usuarios)

--- app/mensajes/page.jsx
+++ app/mensajes/page.jsx
@@ -1,9 +1,26 @@
 'use client'
 
 import React, { useState, useEffect } from "react";
+import { useAuth } from "../context/AuthContext.js"; 
+import { useRouter } from "next/navigation";
 
 
 const Mensajes = () => {
+
+  const { isAuthenticated } = useAuth(); // Obtiene el estado de autenticación
+  const router = useRouter(); // Hook para manejar redirecciones.
+
+  // Redirige al login si no está autenticado
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login"); // Redirige a la página de inicio de sesión.
+    }
+  }, [isAuthenticated, router]);
+
+  // Evita que se muestre contenido mientras redirige
+  if (!isAuthenticated) {
+    return null;
+  }
   
   return (
     <div>Mensajes</div>
--- app/usuarios/page.jsx
+++ app/usuarios/page.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../context/AuthContext.js"; 
+import { useRouter } from "next/navigation";
 
 const Usuarios = () => {
+
+  const { isAuthenticated } = useAuth(); // Obtiene el estado de autenticación
+  const router = useRouter(); // Hook para manejar redirecciones.
+  
+  // Redirige al login si no está autenticado
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login"); // Redirige a la página de inicio de sesión.
+    }
+  }, [isAuthenticated, router]);
+  
+  // Evita que se muestre contenido mientras redirige
+  if (!isAuthenticated) {
+    return null;
+  }
+
   return (
     <div>Usuarios</div>
   )
