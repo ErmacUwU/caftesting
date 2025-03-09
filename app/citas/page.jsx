beba9c137dccd93025ba92a6bc1e2db4748da1ca
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2025-03-08 17:00:33 -0800

    Implementacion de Boton para guardar horario III

--- app/citas/page.jsx
+++ app/citas/page.jsx
@@ -119,16 +119,24 @@ const Citas = () => {
   // Guardar cambios de horario en la base de datos
   const handleSaveSchedule = async () => {
     try {
-      await axios.put("/api/schedule", workSchedule, {
+      const apiUrl =
+        process.env.NODE_ENV === "production"
+          ? "https://cafens.netlify.app/api/schedule"
+          : "/api/schedule"; // En local se usa la ruta relativa
+  
+      const response = await axios.put(apiUrl, workSchedule, {
         headers: {
-          "Content-Type": "application/json", // Indica que el contenido es JSON
+          "Content-Type": "application/json",
         },
       });
+  
+      console.log("Horario actualizado:", response.data);
       setIsScheduleModalOpen(false);
     } catch (error) {
-      console.error("Error al guardar horario:", error);
+      console.error("Error al guardar horario:", error.response?.data || error.message);
     }
   };
+  
 
   const getEventColor = (service) => {
     switch (service) {
