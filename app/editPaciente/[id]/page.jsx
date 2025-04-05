5756299023cbc78541d069970c9e32d2f314b3b0
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2025-04-04 18:14:50 -0700

    Ajustes de ruta IV

--- app/editPaciente/[id]/page.jsx
+++ app/editPaciente/[id]/page.jsx
@@ -1,51 +1,49 @@
+'use client';
+import { useEffect, useState } from "react";
 import ActualizarPaciente from "@/app/components/ActualizarPaciente";
 
-
-const getPatientById = async (id) => {
-  try {
-    const res = await fetch(`api/patient/${id}`, {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch topic");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export default async function updatePatient({ params }) {
+const UpdatePatient = ({ params }) => {
   const { id } = params;
-  const { patient } = await getPatientById(id);
-  const {
-    firstName,
-    lastName,
-    birthdate,
-    gender,
-    patientStatus,
-    birthCity,
-    nationality,
-    birthState,
-    idType,
-    contacts
-  } = patient;
+  const [patientData, setPatientData] = useState(null);
+
+  useEffect(() => {
+    const getPatientById = async () => {
+      try {
+        const res = await fetch(`/api/patient/${id}`, { cache: "no-store" });
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch patient");
+        }
+
+        const data = await res.json();
+        setPatientData(data.patient);
+      } catch (error) {
+        console.error("Error fetching patient:", error);
+      }
+    };
+
+    getPatientById();
+  }, [id]);
+
+  if (!patientData) {
+    return <p>Cargando datos del paciente...</p>;
+  }
 
   return (
     <ActualizarPaciente
-    id={id}
-    firstName={firstName}
-    lastName={lastName}
-    birthdate={birthdate}
-    gender={gender}
-    patientStatus={patientStatus}
-    birthCity={birthCity}
-    nationality={nationality}
-    birthState={birthState}
-    idType={idType}
-    contacts={contacts}
+      id={id}
+      firstName={patientData.firstName}
+      lastName={patientData.lastName}
+      birthdate={patientData.birthdate}
+      gender={patientData.gender}
+      patientStatus={patientData.patientStatus}
+      birthCity={patientData.birthCity}
+      nationality={patientData.nationality}
+      birthState={patientData.birthState}
+      idType={patientData.idType}
+      contacts={patientData.contacts}
     />
   );
-}
+};
+
+export default UpdatePatient;
