12be98ca65f9cdd3f6a47e0e89ba850962e2264e
sistema-caf <sistemacaf0@gmail.com>
2025-09-04 21:56:14 -0700

    Subida inicial del sistema CAF
    Proyecto Completo

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "standalone", // Permite que el proyecto funcione en servidores sin generar HTML estático
+    reactStrictMode: true,
+};
+
+export default nextConfig;
